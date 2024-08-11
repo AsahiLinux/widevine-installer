eab8c668ad3f9db27a444ee1f94b82d8f3ab5336
Hector Martin <marcan@marcan.st>
2024-08-12 00:05:29 +0900

    Use the shell profile in *addition* to environment.d
    Works around https://bugs.kde.org/show_bug.cgi?id=491579
    
    Signed-off-by: Hector Martin <marcan@marcan.st>

---
+++ conf/gmpwidevine.sh
@@ -0,0 +1,4 @@
+if [[ ":$MOZ_GMP_PATH:" != *":/var/lib/widevine/gmp-widevinecdm/system-installed:"* ]]; then
+    MOZ_GMP_PATH="${MOZ_GMP_PATH}${MOZ_GMP_PATH:+:}/var/lib/widevine/gmp-widevinecdm/system-installed"
+    export MOZ_GMP_PATH
+fi
